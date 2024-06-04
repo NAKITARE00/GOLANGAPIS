8a5c138a02300d738b01a3f9122b5ef41f01e5bc
NAKITARE00 <paulnakitare08@gmail.com>
2024-06-04 13:09:38 +0300

    MAIN.GO

--- main.go
+++ main.go
@@ -1,2 +1,44 @@
 package main
 
+import (
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+type User struct {
+	NIN string `json:"nin"`
+
+}
+
+type Question struct {
+	NIN string `json:"nin"`
+	Question string `json:"question"`
+}
+
+type Answer struct {
+	NIN string `json:"nin"`
+	Answer string `json:"answer"`
+}
+
+type Results struct {
+	NIN string `json:"nin"`
+	Results string `json:"results"`
+}
+
+func reqQuestions(w http.ResponseWriter, r *http.Request){}
+
+func recQuestions(w http.ResponseWriter, r *http.Request){}
+
+func retAnswers(w http.ResponseWriter, r *http.Request){
+	getResults()
+}
+
+func getResults(){}
+
+func main(){
+	router := mux.NewRouter()
+	router.HandleFunc("/user", reqQuestions).Methods("POST")
+	router.HandleFunc("/verification", recQuestions).Methods("GET")
+	router.HandleFunc("/results", retAnswers).Methods("GET")
+}
